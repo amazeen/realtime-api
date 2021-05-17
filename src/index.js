5e1afdb0fb6d0c737b92425291da2a1431b84b77
Nicola Bovolato <nicola.bovolato@gmail.com>
2021-05-17 16:37:33 +0200

    refactored index.js

--- src/index.js
+++ src/index.js
@@ -1,83 +1,82 @@
-/**
- *
- *
- *
- *
- */
 require('dotenv').config()
 
-const amqp = require("amqplib/callback_api");
+const amqp = require("amqplib")
 
 const socketioPort = process.env.PORT || 80
+const rabbitMqUrl  = process.env.RABBITMQ_URL || ''
+
+const rabbitMqExchange = 'parameters'
+const rabbitMqTimeout = 5
+
+const initRabbitMq = async () => {
+
+    try{
+        
+        const connection = await amqp.connect(rabbitMqUrl)
+        const channel = await connection.createChannel()
+
+        channel.assertExchange(rabbitMqExchange, 'fanout', { durable: false })
+        const {queue} = channel.assertQueue('', { durable: false, exclusive: true })
+
+        channel.bindQueue(queue, rabbitMqExchange, '')
+        channel.consume(queue, onMessageReceived, { noAck: true })  
+    }
+    catch(err) {
+        console.log(err)
+        console.warn('Error connecting to rabbitmq service, retrying in %s seconds', rabbitMqTimeout)
+        setTimeout(initRabbitMq, rabbitMqTimeout * 1000)
+    }
+}
+
+const onMessageReceived = async(msg) => {
+    
+    try {
+        const { type, data } = JSON.parse(msg.content)
+
+        switch (type) {
+            case "parameter:reading":
+                sendMessage(data.silo, type, {type: data.type, value: data.value, active: data.active})
+                break;
+
+            case "parameter:threshold-reached":
+                sendMessage(data.silo, type, {type: data.type, value: data.value})
+                break;
+
+            default:
+                console.log("unknown message: " + type);
+        }
+    } 
+    catch (err) {
+      console.log(err);
+    }
+}
+
+const sendMessage = async(room, type, data) => {
+    io.to(room).emit(type, data)
+}
 
 const io = require("socket.io")(socketioPort, {
-  cors: {
-    origin: "*",
-  },
-});
-
-console.log('listening on port ' + socketioPort)
+    cors: {
+        origin: "*",
+    }
+})
 
 io.on("connection", (socket) => {
 
-  socket.on("silo:join", (silo) => {
-    socket.join(silo);
-  });
-
-  socket.on("silo:leave", (silo) => {
-    socket.leave(silo);
-  });
-});
-
-amqp.connect(process.env.RABBITMQ_URL, (error0, connection) => {
-  if (error0) {
-    console.log(error0);
-    //throw error0;
-  }
-
-  connection.createChannel((error1, channel) => {
-    if (error1) {
-      throw error1;
-    }
-    const queue = "hello";
-
-    channel.assertQueue(queue, {
-      durable: false,
-    });
-
-    channel.consume(
-      queue,
-      (msg) => {
-        const { type, data } = JSON.parse(msg.content);
-
+    socket.on("silo:join", (silo) => {
         try {
-          switch (type) {
-            case "reading:new":
-              io.to(data.silo).emit("parameter:reading", {
-                type: data.type,
-                value: data.value,
-                active: data.active,
-              });
-
-              break;
-            case "reading:threshold-reached":
-              io.to(data.silo).emit("parameter:threshold-reached", {
-                type: data.type,
-                value: data.value,
-              });
-
-              break;
-            default:
-              console.log("unknown message: " + type);
-          }
-          console.log(" [x] Received %s", msg.content.toString());
-        } catch (err) {
-          console.log(err);
+            socket.join(silo)
         }
-      },
-      {
-        noAck: true,
-      }
-    );
-  });
-});
+        catch(err) {}
+    })
+
+    socket.on("silo:leave", (silo) => {
+        try {
+            socket.leave(silo)
+        }
+        catch(err) {}
+    })
+})
+
+initRabbitMq()
+console.log('listening on port ' + socketioPort)
